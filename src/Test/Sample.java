82ff8118456c977080a46ab64a84e94f32216eb8
ripon <Ripon>
2017-09-22 23:42:58 -0400

    Change

--- src/Test/Sample.java
+++ src/Test/Sample.java
@@ -6,5 +6,6 @@ package Test;
 public class Sample {
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("Hello Java Developer");
     }
 }
